80ec3c8ebdcb3afa5944de4da380b24e39441c7d
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-11-23 09:24:28 +0300

    init base architecture

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { UsersModule } from './users/users.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const users = await NestFactory.create(UsersModule);
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
