8f245d32bce87f041e702e2aca9ebef25a8dec8f
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-12-10 10:39:20 +0300

    fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import * as process from 'node:process';
 
 export const Config = {
   DB_NAME: process.env.DB_NAME || 'bloggers-platform',
-  DB_URI: 'mongodb://localhost/bloggers-platform',
+  DB_URI: process.env.DB_URI || 'mongodb://localhost/bloggers-platform',
   TOKEN_SECRET_KEY: process.env.TOKEN_SECRET_KEY || '111',
   ACCESS_TOKEN_TTL: process.env.ACCESS_TOKEN_TTL || '30min',
   REFRESH_TOKEN_TTL: process.env.REFRESH_TOKEN_TTL || '60min',
